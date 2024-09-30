b73424f68caae1584fa04b283100c99949a006b5
kyerim1 <137040676+kyerim1@users.noreply.github.com>
2024-09-30 15:27:06 +0900

    Update led1.ino

--- led1/led1.ino
+++ led1/led1.ino
@@ -1,12 +1,38 @@
-void setup() {
-  pinMode(8, OUTPUT);
+#define LED1 2
+#define LED2 4
+#define LED3 7
+#define LED4 8
 
+int led[4]={LED1,LED2,LED3,LED4};
+
+void setup() {
+  for( int i=0; i<4; i++){
+    pinMode(led[i], OUTPUT);
+  }
+  
 }
 
 void loop() {
-  digitalWrite(8 , HIGH);
-  delay(1000);
-  digitalWrite(8, LOW);
-  delay(1000);
-
+   for(int i=0; i<4; i++){
+    digitalWrite(led[i], HIGH);
+    for(int k=0; k<4; k++){
+      if(k!=i)
+      digitalWrite(led[k],LOW);
+    }
+    delay(1000);
+  }
 }
+
+
+//void setup() {
+//  pinMode(8, OUTPUT);
+//
+//}
+
+//void loop() {
+//  digitalWrite(8 , HIGH);
+//  delay(1000);
+//  digitalWrite(8, LOW);
+//  delay(1000);
+
+//}
